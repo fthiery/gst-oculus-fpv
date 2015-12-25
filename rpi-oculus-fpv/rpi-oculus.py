e88535970086817e949436b68ed680879be55c86
Florent Thiéry <florent.thiery@ubicast.eu>
2015-12-25 23:21:28 +0100

    minor cleanup to bus message management

--- rpi-oculus-fpv/rpi-oculus.py
+++ rpi-oculus-fpv/rpi-oculus.py
@@ -7,24 +7,21 @@ logger = logging.getLogger('FpvPipeline')
 
 import gi
 gi.require_version('Gst', '1.0')
-
 from gi.repository import GObject, Gst
 
-#source = "v4l2src ! video/x-raw, format=(string)YUY2, width=(int)640, height=(int)360"
+GObject.threads_init()
+Gst.init(None)
+Gst.debug_set_active(True)
+Gst.debug_set_colored(True)
+Gst.debug_set_default_threshold(Gst.DebugLevel.WARNING)
+Gst.debug_set_threshold_for_name("glimage*", 5)
+
+#source = "v4l2src ! video/x-raw, format=(string)YUY2, width=(int)640, height=(int)360, pixel-aspect-ratio=(fraction)1/1, interlace-mode=(string)progressive, colorimetry=(string)1:4:7:1, framerate=(fraction)30/1"
 source = 'videotestsrc ! video/x-raw, format=(string)YUY2, width=(int)720, height=(int)480'
 
 display_pattern = 'tee name=src ! queue name=qtimeoverlay ! timeoverlay name=timeoverlay font-desc="Arial {font_size}" silent=true ! glupload ! glcolorconvert ! glcolorscale ! videorate ! video/x-raw(memory:GLMemory), width=(int){display_width}, height=(int){display_height}, pixel-aspect-ratio=(fraction)1/1, interlace-mode=(string)progressive, framerate=(fraction){render_fps}/1, format=(string)RGBA ! gltransformation name=gltransformation ! glshader location=oculus.frag ! glimagesink name=glimagesink'
 
 encoder_pattern = 'src. ! queue ! videoconvert ! x264enc tune=zerolatency speed-preset=1 bitrate={bitrate_video} ! mp4mux ! filesink location=test.mp4'
-
-def init():
-    GObject.threads_init()
-    Gst.init(None)
-    Gst.debug_set_active(True)
-    Gst.debug_set_colored(True)
-    Gst.debug_set_default_threshold(Gst.DebugLevel.WARNING)
-
-init()
 
 default_config = {
     'headtracker_enable': True,
@@ -97,14 +94,14 @@ class FpvPipeline:
         return Gst.parse_launch(pipeline)
 
     def send_eos(self, *args):
-        logger.info("Sending EOS")
-        event = Gst.Event.new_eos()
-        Gst.Element.send_event(self.pipeline, event)
+        if hasattr(self, "pipeline") and self.is_running():
+            logger.info("Sending EOS")
+            event = Gst.Event.new_eos()
+            Gst.Element.send_event(self.pipeline, event)
+        else:
+            logger.info("No pipeline or pipeline not running, skipping EOS emission")
+            self.run_actions_after_eos()
 
-    def on_eos(self):
-        logger.info("Got EOS")
-        self.pipeline.set_state(Gst.State.NULL)
-        self.run_actions_after_eos()
 
     def run_actions_after_eos(self):
         for action in self.actions_after_eos:
@@ -121,24 +118,33 @@ class FpvPipeline:
     def activate_bus(self):
         self.bus = self.pipeline.get_bus()
         self.bus.add_signal_watch()
+        
         self.bus.connect('message', self._on_message)
+
+        self.bus.connect('message::eos', self._on_eos)
+        self.bus.connect('message::error', self._on_error)
 
     def activate_frame_callback(self):
         sink = self.pipeline.get_by_name('glimagesink')
         sink.connect("client-draw", self._on_draw)
 
     def _on_draw(self, src, glcontext, sample, *args):
-        print('Frame')
+        #print('Frame')
+        pass
+
+    def _on_eos(self, bus, message):
+        logger.info("Got EOS")
+        self.pipeline.set_state(Gst.State.NULL)
+        self.run_actions_after_eos()
+
+    def _on_error(self, bus, message):
+        err, debug = message.parse_error()
+        error_string = "{0} {1}".format(err, debug)
+        logger.error("Error: {0}".format(error_string))
 
     def _on_message(self, bus, message):
         t = message.type
-        if t == Gst.MessageType.ERROR:
-            err, debug = message.parse_error()
-            error_string = "{0} {1}".format(err, debug)
-            logger.info("Error: {0}".format(error_string))
-        elif t == Gst.MessageType.EOS:
-            self.on_eos()
-        elif t == Gst.MessageType.ELEMENT:
+        if t == Gst.MessageType.ELEMENT:
             name = message.get_structure().get_name()
             res = message.get_structure()
             source = message.src.get_name()  # (str(message.src)).split(":")[2].split(" ")[0]
@@ -162,8 +168,8 @@ if __name__ == '__main__':
 
     f = FpvPipeline()
     GObject.idle_add(f.start)
-    GObject.timeout_add_seconds(10, f.toggle_record)
-    GObject.timeout_add_seconds(20, f.toggle_record)
+    #GObject.timeout_add_seconds(10, f.toggle_record)
+    #GObject.timeout_add_seconds(20, f.toggle_record)
     ml = GObject.MainLoop()
 
     def signal_handler(signal, frame):
