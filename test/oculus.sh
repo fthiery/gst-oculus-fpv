8326192381f9d189e2e3098323d90bf130ee7875
Florent Thiéry <florent.thiery@ubicast.eu>
2015-12-27 22:09:33 +0100

    use videotestsrc instead of v4l2src

--- test/oculus.sh
+++ test/oculus.sh
@@ -2,4 +2,4 @@
 # Copyright 2015, Florent Thiery 
 CMD=gst-gtklaunch-1.0
 #CMD=gst-launch-1.0
-$CMD v4l2src ! video/x-raw\,\ format\=\(string\)YUY2\,\ width\=\(int\)640\,\ height\=\(int\)360 ! queue ! glupload ! glcolorconvert ! glcolorscale ! video/x-raw\(memory:GLMemory\)\,\ width\=\(int\)1280\,\ height\=\(int\)800\,\ pixel-aspect-ratio\=\(fraction\)1/1\,\ interlace-mode\=\(string\)progressive\,\ framerate\=\(fraction\)30/1\,\ format\=\(string\)RGBA ! gltransformation ! glshader location=oculus.frag ! glimagesink
+$CMD videotestsrc ! video/x-raw\,\ format\=\(string\)YUY2\,\ width\=\(int\)640\,\ height\=\(int\)360 ! queue ! glupload ! glcolorconvert ! glcolorscale ! video/x-raw\(memory:GLMemory\)\,\ width\=\(int\)1280\,\ height\=\(int\)800\,\ pixel-aspect-ratio\=\(fraction\)1/1\,\ interlace-mode\=\(string\)progressive\,\ framerate\=\(fraction\)30/1\,\ format\=\(string\)RGBA ! gltransformation ! glshader location=oculus.frag ! glimagesink
